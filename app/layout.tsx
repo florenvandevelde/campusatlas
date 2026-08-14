e4faa38a56a2a9122e3c5db209acf3d2decb7771
Floren Vandevelde <f.vandevelde@pnptc.com>
2026-08-14 13:44:50 -0700

    Add Vercel Speed Insights

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
